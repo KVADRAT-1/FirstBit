75e7b38a569b884fdecb18e29bd6501a0f005352
KVADRAT <pfzrncywumlejtxbigqvoakshd@gmail.com>
2021-11-13 12:51:27 +0300

    level-0.3.4

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 const { log } = console
+
+$(document).ready(function () {
+    $('.header__burger').click(function (event) {
+        $('.header__burger, .header__menu, .header').toggleClass('active')
+    })
+})
