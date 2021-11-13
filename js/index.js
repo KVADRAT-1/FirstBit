8cff47a06d1a60ef201d44bdc5aa34752500f5e4
KVADRAT <pfzrncywumlejtxbigqvoakshd@gmail.com>
2021-11-13 20:12:36 +0300

    level-0.4

--- js/index.js
+++ js/index.js
@@ -5,3 +5,38 @@ $(document).ready(function () {
         $('.header__burger, .header__menu, .header').toggleClass('active')
     })
 })
+
+const sections = $('section'),
+    nav = $('nav'),
+    nav_height = nav.outerHeight()
+
+$(window).on('scroll', function () {
+    const cur_pos = $(this).scrollTop()
+
+    sections.each(function () {
+        const top = $(this).offset().top - nav_height,
+            bottom = top + $(this).outerHeight()
+
+        if (cur_pos >= top && cur_pos <= bottom) {
+            nav.find('a').removeClass('active')
+            sections.removeClass('active')
+
+            $(this).addClass('active')
+            nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active')
+        }
+    })
+})
+
+nav.find('a').on('click', function () {
+    const $el = $(this),
+        id = $el.attr('href')
+
+    $('html, body').animate(
+        {
+            scrollTop: $(id).offset().top - nav_height + 1,
+        },
+        500
+    )
+
+    return false
+})
