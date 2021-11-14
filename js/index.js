b140dc430bfe58e5d0c86c81fb9b96e6ba5f47a3
KVADRAT <pfzrncywumlejtxbigqvoakshd@gmail.com>
2021-11-14 22:17:41 +0300

    level-0.9.0

--- js/index.js
+++ js/index.js
@@ -1,55 +1,57 @@
-const { log } = console
+const { log } = console;
 const sections = $('section'),
     nav = $('nav'),
     nav_height = nav.outerHeight(),
     newsletter__input = $('.newsletter__input'),
-    newsletter__button = $('.newsletter__button')
+    newsletter__button = $('.newsletter__button');
 
 $(document).ready(function () {
     $('.header__burger').click(function (event) {
-        $('.header__burger, .header__menu, .header').toggleClass('active')
-    })
-})
+        $('.header__burger, .header__menu, .header').toggleClass('active');
+    });
+});
 
 $(window).on('scroll', function () {
-    const cur_pos = $(this).scrollTop()
+    const cur_pos = $(this).scrollTop();
 
     sections.each(function () {
         const top = $(this).offset().top - nav_height,
-            bottom = top + $(this).outerHeight()
+            bottom = top + $(this).outerHeight();
 
         if (cur_pos >= top && cur_pos <= bottom) {
-            nav.find('a').removeClass('active')
-            sections.removeClass('active')
+            nav.find('a').removeClass('active');
+            sections.removeClass('active');
 
-            $(this).addClass('active')
-            nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active')
+            $(this).addClass('active');
+            nav.find('a[href="#' + $(this).attr('id') + '"]').addClass(
+                'active'
+            );
         }
-    })
-})
+    });
+});
 
 nav.find('a').on('click', function () {
     const $el = $(this),
-        id = $el.attr('href')
+        id = $el.attr('href');
     $('html, body').animate(
         {
             scrollTop: $(id).offset().top - nav_height + 1,
         },
         500
-    )
+    );
 
-    return false
-})
+    return false;
+});
 
 $(newsletter__button).on('click', function (e) {
-    e.preventDefault()
+    e.preventDefault();
     const regex =
-        /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/
-    const text__input = $(newsletter__input).val()
+        /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    const text__input = $(newsletter__input).val();
     if (regex.test(text__input)) {
-        alert(text__input)
-        $(newsletter__input).val('')
+        alert(text__input);
+        $(newsletter__input).val('');
     } else {
-        alert('invalid email')
+        alert('invalid email');
     }
-})
+});
